7238c7d2ce2cc21af412dcc24706849fd3149aba
Hanna Garcia Guerra <hnngarcia11@gmail.com>
2025-09-27 11:56:09 -0500

    Fase 2: Implementadas funciones para recibir daño y ganar experiencia

--- 1015068349-Hanna-Garcia-Guerr/app.js
+++ 1015068349-Hanna-Garcia-Guerr/app.js
@@ -1,9 +1,26 @@
 let nombreHeroe = prompt("Ingrese el nombre de su heroe");
-let puntosSalud = 100;
+let puntosSalud = 50;
 let puntosExperiencia = 0;
 
 function mostrarEstadisticas() {
     console.log("Héroe:" + nombreHeroe, "Salud:" + puntosSalud, "Experiencia:" + puntosExperiencia);
 }
 
+//mostrarEstadisticas();
+
+const recibirDanio = function(cantidadDanio) {
+    puntosSalud -= cantidadDanio; 
+    if(puntosSalud <= 0) {
+        console.log("¡Has sido derrotado!");
+        console.log("Recibiste", + cantidadDanio + "de daño." + "Salud restante: " + puntosSalud);
+    }
+}
+
+const ganarExperiencia = (cantidadExp) => {
+    puntosExperiencia += cantidadExp; 
+    console.log("¡Ganaste", + cantidadExp + "de experiencia!." + "XP total: " + puntosExperiencia);
+}
+
+recibirDanio(100);
+ganarExperiencia(15);
 mostrarEstadisticas();
