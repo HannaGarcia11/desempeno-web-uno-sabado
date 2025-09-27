bd0f2c2da5309fabe5793587b33e393b01e573ad
Hanna Garcia Guerra <hnngarcia11@gmail.com>
2025-09-27 11:21:51 -0500

    Fase 1: Configuración inicial y estadísticas del héroe completadas

---
+++ 1015068349-Hanna-Garcia-Guerr/app.js
@@ -0,0 +1,9 @@
+let nombreHeroe = prompt("Ingrese el nombre de su heroe");
+let puntosSalud = 100;
+let puntosExperiencia = 0;
+
+function mostrarEstadisticas() {
+    console.log("Héroe:" + nombreHeroe, "Salud:" + puntosSalud, "Experiencia:" + puntosExperiencia);
+}
+
+mostrarEstadisticas();
