825f59f7fa650163676211721eb52e94bf95ae4e
Hanna Garcia Guerra <hnngarcia11@gmail.com>
2025-09-27 13:50:31 -0500

    Fase 3: Añadida simulación de batalla y diagnóstico del héroe.

--- 1015068349-Hanna-Garcia-Guerr/app.js
+++ 1015068349-Hanna-Garcia-Guerr/app.js
@@ -21,6 +21,53 @@ const ganarExperiencia = (cantidadExp) => {
     console.log("¡Ganaste", + cantidadExp + "de experiencia!." + "XP total: " + puntosExperiencia);
 }
 
-recibirDanio(100);
-ganarExperiencia(15);
-mostrarEstadisticas();
+//recibirDanio(100);
+//ganarExperiencia(15);
+//mostrarEstadisticas();
+
+const simularBatalla = () => {
+    for(let i = 1; i <= 3; i++) {
+        console.log("Turno " + i + ":");
+        recibirDanio(25);
+    if (puntosSalud <= 0) {
+        console.log("La batalla terminó en el turno " + i);
+        return;
+    }
+}
+    if (puntosSalud > 0) {
+        ganarExperiencia(100);
+    }
+};
+
+function diagnosticoHeroe() {
+    if(puntosSalud > 70) {
+        console.log("Tu salud es excelente.");
+    } else if(puntosSalud > 40) {
+        console.log("Estás herido, considera usar una pocion.");
+    } else if(puntosSalud > 0) {
+        console.log("¡Peligro! Salud crítica.");
+    } else {
+        console.log("Has sido derrotado.");
+    }
+
+    switch(puntosExperiencia) {
+        case 10:
+            console.log("Eres un novato.");
+            break;
+        case 50:
+            console.log("Te estás convirtiendo en un guerrero respetable.");
+            break;
+        case 100:
+            console.log("Eres una leyenda viviente.");
+            break;
+    }   
+}
+
+simularBatalla();
+diagnosticoHeroe();
+
+
+
+
+
+
